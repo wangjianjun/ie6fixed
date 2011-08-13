d94f841703b9c7411ec23a7f01c0e035fe15b798
lifehacker007 <jianjun768@gmail.com>
2011-08-13 11:55:58 +0800

    fisrt commit

---
+++ ie6fixed-common.js
@@ -0,0 +1,60 @@
+﻿/**
+ * @fileOverview ie6下fixed定位
+ * @copyright (c) 2011 
+ * @author jianjun768@gmail.com
+ * @version 1.0
+ * @depend jquery.js 
+ */
+ 
+(function($){
+	var ie6Fixed = {
+		/**
+		 * fixed定位
+		 * @param {jquery object} obj 
+		 * @param {string / function} position 定位方式，position可以是cc lt rt lb rb字符串，也可以是自定义的定位函数 
+		 */
+		fixed: function(obj, position){
+			var obj = obj, fixed;
+
+			fixed = function( pos ){
+				obj[0].style.removeExpression('top');
+				var dom = '(document.documentElement || document.body)', ch;
+				
+				switch(position){
+					case "lt" : 
+					case "rt" :
+						obj[0].style.setExpression("top", 'eval('+ dom + '.scrollTop + ' + 0 + ') + "px"');	
+						break ;
+					case "cc" :
+						ch = window.innerHeight || (document.documentElement || document.body).clientHeight;
+						obj[0].style.setExpression("top", 'eval('+ dom + '.scrollTop + ' + (ch / 2) + ') + "px"');
+						break ;
+					case "lb" :
+					case "rb" :
+						obj[0].style.setExpression("top", 'eval('+ dom + '.scrollTop + ' + ($(window).height()-obj.outerHeight()) + ') + "px"');
+						break ;
+					default:
+						obj[0].style.setExpression("top", 'eval('+ dom + '.scrollTop + ' + obj.offset().top + ') + "px"');
+				}
+			}
+			if( $.isFunction(position) ){
+				fixed = position;
+			}
+
+			if($.browser.msie && parseInt($.browser.version) <= 6){
+				var html = document.getElementsByTagName('html')[0];
+				if (document.body.currentStyle.backgroundAttachment !== 'fixed') {
+					html.style.backgroundImage = 'url(about:blank)';
+					html.style.backgroundAttachment = 'fixed';
+				};
+				obj.css("position","absolute");
+				fixed( position );
+				$(window).bind("resize", function(){
+					fixed( position );
+				});
+			}
+		}
+	}
+	window.ie6Fixed = ie6Fixed;
+})(jQuery);
+
